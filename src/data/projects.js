d2beaee6f849ea9789d6205cb0c5166f074a74ea
trmorningstar <trmorningstar@gmail.com>
2022-10-15 09:28:47 -0400

    add project data

---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+[{title: 'Mortal Connect Four Combat', 
+  description:"Play the classic strategy game, 'Connect Four' with the aesthetics of the video game Mortal Kombat.",
+  image:"",
+  repositoryLink:"https://github.com/tiffanymorningstar/connect-four.git",
+  deploymentLink:"https://tiffany-morningstar-connect-four.netlify.app"
+}
+{title:"Migraine Tracker", 
+  description:"An app that gives you the ability to add migraines and triggers, so that you can track them over time and determine better ways to treat and prevent them.",
+  image:"",
+  repositoryLink:"https://github.com/tiffanymorningstar/migraine-tracker.git",
+  deploymentLink:"https://migrainetracker.fly.dev"
+}
+
+]
