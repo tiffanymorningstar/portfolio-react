de3979c7a029a4dda765708bbcb8467c20e34a59
trmorningstar <trmorningstar@gmail.com>
2022-11-25 19:11:26 -0500

    change project order

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,10 @@
 
 
-const projects = [{title: 'Mortal Connect Four Combat', 
-  description: "Connect Four with the look of Mortal Kombat.",
-  image:"https://i.imgur.com/TTcXiLU.png",
-  repositoryLink:"https://github.com/tiffanymorningstar/connect-four.git",
-  deploymentLink:"https://tiffany-morningstar-connect-four.netlify.app"
+const projects = [{title: "AVI Birding", 
+description:"An app for Bird Watchers",
+image:"https://i.imgur.com/uYDnmni.png",
+repositoryLink:"https://github.com/tiffanymorningstar/Avi-front-end.git",
+deploymentLink:"https://avi-birding.netlify.app/"
 },
 {title: "Migraine Tracker", 
   description:"An app for tracking migraines and triggers",
@@ -12,14 +12,12 @@ const projects = [{title: 'Mortal Connect Four Combat',
   repositoryLink:"https://github.com/tiffanymorningstar/migraine-tracker.git",
   deploymentLink:"https://migrainetracker.fly.dev"
 },
-{title: "AVI Birding", 
-  description:"An app for Bird Watchers",
-  image:"https://i.imgur.com/uYDnmni.png",
-  repositoryLink:"https://github.com/tiffanymorningstar/Avi-front-end.git",
-  deploymentLink:"https://avi-birding.netlify.app/"
+{title: 'Mortal Connect Four Combat', 
+  description: "Connect Four with the look of Mortal Kombat.",
+  image:"https://i.imgur.com/TTcXiLU.png",
+  repositoryLink:"https://github.com/tiffanymorningstar/connect-four.git",
+  deploymentLink:"https://tiffany-morningstar-connect-four.netlify.app"
 }
-
-
 ]
 
 export { projects };
